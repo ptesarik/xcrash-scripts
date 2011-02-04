ad18d171c73956e834f15d38359cbd2a3031b6bb
Petr Tesarik <petr@tesarici.cz>
2011-02-04 12:35:59 +0100

    simple-subst.pl: allow raw matches
    The default is to match only words, i.e. the search expression
    is delimited by word boundaries on both sides. Add an "-r" option
    to override this.

--- simple-subst.pl
+++ simple-subst.pl
@@ -1,9 +1,14 @@
 #! /usr/bin/perl
 
 my $old = shift @ARGV;
+if ($old eq "-r") {		# "raw" regexp
+    $old = shift @ARGV;
+} else {
+    $old = "\\b$old\\b";	# otherwise delimited by word boundaries
+}
 my $new = shift @ARGV;
 
 while (<>) {
-    s/\b$old\b/$new/go;
+    s/$old/$new/go;
     print;
 }
