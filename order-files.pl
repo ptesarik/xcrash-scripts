634e2e2d74fafff84b01f802839ca2916f01bdea
Petr Tesarik <petr@tesarici.cz>
2011-02-16 11:59:00 +0100

    order-file.pl: order source files so that they can be parsed
    Unfortunatelly, there are some clashes in the various definitions,
    so it's necessary to choose the right order of parsing. The algorithm
    is based on trial and error, and is subject to change.

---
+++ order-files.pl
@@ -0,0 +1,77 @@
+#! /usr/bin/perl
+
+# Unfortunately, the sources cannot be parsed in any order...
+# The following has proved productive:
+#   1. Move *.h to begin
+#   2. Move defs.h to begin
+#   3. Move lkcd* to end
+#   4. Move unwind* to end
+#   5. Move unwind_i.h to end
+
+undef $/;
+my @list = split(/\s+/, <>);
+
+my @begin;
+my @end;
+
+# 1. Move *.h to begin
+foreach my $file (@list) {
+    if ($file =~ /\.h$/) {
+	push @begin, $file;
+    } else {
+	push @end, $file;
+    }
+}
+@list = ( @begin, @end );
+undef @begin;
+undef @end;
+
+# 2. Move defs.h to begin
+foreach my $file (@list) {
+    if ($file eq "defs.h") {
+	push @begin, $file;
+    } else {
+	push @end, $file;
+    }
+}
+@list = ( @begin, @end );
+undef @begin;
+undef @end;
+
+# 3. Move lkcd* to end
+foreach my $file (@list) {
+    if ($file =~ /^lkcd/) {
+	push @end, $file;
+    } else {
+	push @begin, $file;
+    }
+}
+@list = ( @begin, @end );
+undef @begin;
+undef @end;
+
+# 4. Move unwind* to end
+foreach my $file (@list) {
+    if ($file =~ /^unwind/) {
+	push @end, $file;
+    } else {
+	push @begin, $file;
+    }
+}
+@list = ( @begin, @end );
+undef @begin;
+undef @end;
+
+# 5. Move unwind_i.h to end
+foreach my $file (@list) {
+    if ($file eq "unwind_i.h") {
+	push @end, $file;
+    } else {
+	push @begin, $file;
+    }
+}
+@list = ( @begin, @end );
+undef @begin;
+undef @end;
+
+print join(" ", @list), "\n";
