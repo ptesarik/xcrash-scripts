ptesarik/xcrash-scripts

commit f900e03245d9bb679b739bc68e2c2fbcc5863773

Treat function and array types as pointers in subst_target_var()
This place was still missing the check, causing various type mismatches.

xform.c
@@ -505,6 +505,10 @@ subst_target_var(node_t *firstvar, ind_t *ind)
 	int ret = 0;
 	do {
 		node_t *type = first_node(&var->child[chv_type]);
+		if (*ind == ind_pointer &&
+		    (type->t.category == type_func ||
+		     type->t.category == type_array))
+			--ind;
 		ret += subst_target_type(type, ind);
 	} while ((var = next_dup(var)) != firstvar);
 	return ret;

commit a96eb84811e944f710584aea2e34a25349dc7b30

Provide a stub indirection instead of NULL
A NULL pointer for the @ind argument was originally handled specially.
Commit f900e03245d9bb679b739bc68e2c2fbcc5863773 broke this. Since it
is likely that this will break again sooner or later, pass a pointer
to an empty indirection instead.

xform.c
@@ -452,7 +452,7 @@ next_dup(node_t *node)
 static int
 subst_target_type(node_t *type, const ind_t *ind)
 {
-	while (ind && *ind != ind_stop) {
+	while (*ind != ind_stop) {
 		if (*ind == ind_pointer) {
 			if (type->t.category != type_pointer) {
 				ind_warn("pointer not found", ind);
@@ -857,6 +857,7 @@ find_assign(node_t *node)
 static enum walk_action
 target_facilitators(node_t *node, void *data)
 {
+	static const ind_t no_ind = ind_stop;
 	struct parsed_file *pf = data;
 
 	if (!is_direct_call(node, "INT")
@@ -878,7 +879,7 @@ target_facilitators(node_t *node, void *data)
 	if (!var || list_empty(&var->child[chv_type]))
 		return walk_continue;
 
-	subst_target_var(var, NULL);
+	subst_target_var(var, &no_ind);
 
 	return walk_continue;
 }